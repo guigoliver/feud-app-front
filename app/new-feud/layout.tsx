8a1e74010f77d2d4a2171887bcc3ba99bb40128b
Guilherme Afonso <profguilherme.as@gmail.com>
2024-07-04 17:47:40 -0300

    feat: create feud page layout

--- app/new-feud/layout.tsx
+++ app/new-feud/layout.tsx
@@ -1,9 +1,12 @@
 import { ReactNode } from "react"
 
-const NewFeudLayout = ({children}: { children: ReactNode}) => {
+const NewFeudLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <main className="flex flex-col items-center justify-between p-24">
-            {children}
+        <main className="bg-red-500 bg-no-repeat bg-cover bg-center w-full h-screen" style={{backgroundImage: 'url(/images/parchment-bg.jpg)' }}>
+            <section className="flex flex-col items-center justify-between p-24">
+                {children}
+
+            </section>
         </main>
     )
 }
--- app/new-feud/page.tsx
+++ app/new-feud/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import {
     Card,
@@ -9,9 +10,13 @@ import {
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-
+import Link from 'next/link'
 
 const NewFeud = () => {
+    const handleSubmit = () => {
+        console.log('submit')
+    }
+
     return (
         <div>
             <Card className='flex flex-col items-center justify-center gap-4'>
@@ -19,23 +24,33 @@ const NewFeud = () => {
                     <CardTitle>Create New Feud</CardTitle>
                 </CardHeader>
                 <CardContent className="flex flex-col justify-start gap-2">
-                    <form>
-                        <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <form className="flex flex-col justify-start gap-2">
+                        <div className="grid w-full items-center gap-1.5">
                             <label htmlFor="name">Feud Name</label>
-                            <Input id="name" type="text" className="w-full focus:outline-none focus:ring-none"/>
+                            <Input id="name" type="text" className="w-full border-none focus:outline-blue-500 focus-visible:outline-none focus-visible:ring-blue-500 focus:ring-none focus:border-blue-500"/>
                         </div>
-                        <label>Feud Description</label>
-                        <input className='w-full border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500' />
-                        <div>
-                            <label>Width</label>
-                            <input />
-                            <label>Length</label>
-                            <input />
+                        <div className="grid w-full items-center gap-1.5">
+                        <label htmlFor="description">Feud Description</label>
+                        <Input id="description" type="text" className="w-full border-none focus:outline-blue-500 focus-visible:outline-none focus-visible:ring-blue-500 focus:ring-none focus:border-blue-500"/>
+
+                        </div>
+                        <div className="flex flex-row gap-2">
+                            <div className="grid w-full items-center gap-1.5">
+                            <label htmlFor="width">Width</label>
+                            <Input id="width" type="number" className="w-full border-none focus:outline-blue-500 focus-visible:outline-none focus-visible:ring-blue-500 focus:ring-none focus:border-blue-500"/>
+                            </div>
+                            
+                            <div className="grid w-full items-center gap-1.5">
+                            <label htmlFor="length">Length</label>
+                            <Input id="length" type="number" className="w-full border-none focus:outline-blue-500 focus-visible:outline-none focus-visible:ring-blue-500 focus:ring-none focus:border-blue-500"/>
+                            </div>
                         </div>
                     </form>
                 </CardContent>
                 <CardFooter className="w-full flex justify-end">
-                    <Button>Save</Button>
+                    <Button asChild type='submit' onClick={() => handleSubmit()}>
+                        <Link href='/feuds-list'>Create</Link>
+                    </Button>
                 </CardFooter>
             </Card>
 
